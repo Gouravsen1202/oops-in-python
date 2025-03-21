efc0b4eeb97a6db7f92531663c0925b151262b6f
Gouravsen1202 <gouravsen757@gmail.com>
2025-03-21 18:27:24 +0530

    new code

---
+++ encapsulation/encap.py
@@ -0,0 +1,11 @@
+class abc:
+    def fun(self,a):
+        self.x=a
+    def fun2(self):
+        print(self.x)#yaha data ko pass kiya 
+obj=abc()
+obj.fun("jay")# yha huumne data ko buinding kya
+
+obj.fun2()add
+
+
